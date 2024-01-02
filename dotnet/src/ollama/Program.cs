afc00b9ab5bac67433f2cd7a448250e4309c732d
jgilbert <gilbertj@moodys.com>
2024-01-02 16:41:57 +0000

    2-ollama-and-dotnet
    WIP dotnet interpretation

--- dotnet/src/ollama/Program.cs
+++ dotnet/src/ollama/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+using Kernel = Microsoft.SemanticKernel.Kernel;
+using Secrets;
+
+bool useAzureOpenAI = false;
+string model = "gpt-3.5-turbo";
+Secrets localSecrets = new Secrets { };
+List<string>[] apiKeys = new List<string>
+{localSecrets.apiKey};
+
+//Create Kernel builder
+var builder = Kernel.CreateBuilder();
+
+// Configure AI service credentials used by the kernel
+// var (model, apiKey) = Settings.LoadFromFile();
+
+/*
+if (useAzureOpenAI)
+    builder.AddAzureOpenAIChatCompletion(model, azureEndpoint, apiKey);
+else
+    builder.AddOpenAIChatCompletion(model, apiKeys);
+    */
+
+builder.AddOpenAIChatCompletion(model, apiKeys);
+
+var kernel = builder.Build();
